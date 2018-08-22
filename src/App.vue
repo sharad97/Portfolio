8df8f18decb26f4205c0a7ab19b0f15f5c4610e5
Sharad Shrestha <sshrest3@ucsc.edu>
2018-08-22 04:24:05 -0700

    Fixed v-btn issue with Firefox

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="sidebar" app>
-    <v-list>
-    <div v-for="item in  menuItems">
-        <router-link v-if="item.path" v-bind:to="item.path">
-          <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
-            <v-list-tile-action>
-              <v-icon style="font-size:24px">{{ item.action }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <a v-else v-bind:href="item.link" target="_blank">
-          <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
-            <v-list-tile-action>
-              <v-icon style="font-size:24px">{{ item.action }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </a>
-      </div>
-      <v-toolbar-side-icon @click="sidebar = !sidebar">
-              <v-icon style="font-size:24px">fa fa-times-circle</v-icon>
-      </v-toolbar-side-icon>
-    </v-list>
+      <v-list>
+        <div v-for="item in  menuItems">
+            <router-link v-if="item.path" v-bind:to="item.path">
+              <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
+                <v-list-tile-action>
+                  <v-icon color="blue darken-2" style="font-size:24px">{{ item.action }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </router-link>
+            <a v-else v-bind:href="item.link" target="_blank">
+              <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
+                <v-list-tile-action>
+                  <v-icon color="blue darken-2" style="font-size:24px">{{ item.action }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </a>
+          </div>
+          <v-toolbar-side-icon @click="sidebar = !sidebar">
+            <v-icon style="font-size:24px">fa fa-times-circle</v-icon>
+          </v-toolbar-side-icon>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar app>
       <span class="hidden-sm-and-up">
@@ -37,45 +37,47 @@
       </span>
       <v-toolbar-items>
         <div>
-        <v-btn flat color="purple">
         <router-link to="/" tag="span" style="cursor: pointer">
           <v-list-tile>
-            <v-list-tile-action>
-              <v-icon style="font-size:24px">fa fa-user-circle-o</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ appTitle }}</v-list-tile-title>
-            </v-list-tile-content>
+            <v-btn flat color="purple">
+              <v-list-tile-action>
+                <v-icon style="font-size:24px">fa fa-user-circle-o</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="text1">{{ appTitle }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-btn>
           </v-list-tile>
         </router-link>
-        </v-btn>
          </div>
       </v-toolbar-items>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
         <div v-for="item in  menuItems">
-        <v-btn flat color="purple">
         <router-link v-if="item.path" v-bind:to="item.path">
           <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
-            <v-list-tile-action>
-              <v-icon style="font-size:24px">{{ item.action }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
+            <v-btn flat color="purple">
+              <v-list-tile-action>
+                <v-icon color="blue darken-2" style="font-size:24px">{{ item.action }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="text">{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-btn>
           </v-list-tile>
         </router-link>
         <a v-else v-bind:href="item.link" target="_blank">
           <v-list-tile v-if="item.action" v-bind:key="item.title" @click="">
-            <v-list-tile-action>
-              <v-icon style="font-size:24px">{{ item.action }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
+            <v-btn flat color="purple">
+              <v-list-tile-action>
+                <v-icon color="blue darken-2" style="font-size:24px">{{ item.action }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="text">{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-btn>
           </v-list-tile>
         </a>
-        </v-btn>
       </div>
       </v-toolbar-items>
     </v-toolbar>
@@ -87,6 +89,16 @@
 
 <style lang="scss">
 @import './sass/app';
+  
+  .text{
+    color: #1976d2;
+    font-size: 16px;
+  }
+  
+  .text1{
+    font-size: 16px;
+  }
+  
 </style>
 
 <style>
